712a03da9cd740d5e13109cb7a6f81791d047006
Ziaullah Khan <zkhan@capacityllc.com>
2026-02-10 10:49:04 +0530

    Day 6 - Delegates Demo - First example

---
+++ Day-6/DelegatesDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DelegatesDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DelegatesDemoApp app = new DelegatesDemoApp();
+        app.Run();
+    }
+}
+
+class DelegatesDemoApp
+{
+    //void Add(int a, int b)
+    delegate void MathOperation(int a, int b);
+
+    public void Run()
+    {
+        MathOperation operation = Subtract;
+
+        operation(5, 3);
+    }
+
+    public void Add(int a, int b)
+    {
+        Console.WriteLine($"The sum of {a} and {b} is: {a + b}");
+    }
+
+    public void Subtract(int a, int b)
+    {
+        Console.WriteLine($"The difference between {a} and {b} is: {a - b}");
+    }
+}
