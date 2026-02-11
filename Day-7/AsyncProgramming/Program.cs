16a16449eb9328c029cfbba61c71a77d8f88ba71
Ziaullah Khan <zkhan@capacityllc.com>
2026-02-11 17:12:32 +0530

    Logger

---
+++ Day-7/AsyncProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Fetching...");
+// var data = FetchData();  // App frozen for 2 seconds
+var data = await FetchDataAsync();  // App frozen for 2 seconds
+Console.WriteLine($"Got: {data}");
+
+string FetchData()
+{
+    Thread.Sleep(5000);  // Freezes for 2 seconds!
+    return "Data";
+}
+
+async Task<string> FetchDataAsync()
+{
+    await Task.Delay(5000);  // Does NOT freeze the app!
+    return "Data";
+}
