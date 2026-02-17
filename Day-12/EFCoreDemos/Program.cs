54eddf5394e4555343969d4d14047f3c749c2dfd
Ziaullah Khan <zkhan@capacityllc.com>
2026-02-17 15:18:54 +0530

    Day 12 - EF Core - Code First Demo

--- Day-12/EFCoreDemos/Program.cs
+++ Day-12/EFCoreDemos/Program.cs
@@ -18,6 +18,8 @@
 //     public decimal Salary { get; set; }
 // }
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 
@@ -27,6 +29,32 @@ var customers = _context.Customers
     .Where(e => e.Age > 20)
     .ToList();
 
+// Without ToList()
+// Console.WriteLine("customers.GetType(): " + customers.GetType());
+// Console.WriteLine($"Query: {customers.ToQueryString()}");
+
+// With ToList()
+// Console.WriteLine("customers.GetType(): " + customers.GetType());
+
+// With AsEnumerable()
+// Console.WriteLine("customers.GetType(): " + customers.GetType());
+
+// customers.Add(new Customer { Name = "John Doe", Age = 30 });
+// _context.SaveChanges();
+
+Console.WriteLine($"Customers Count: {customers.Count()}");
+
+customers.Add(new Customer { Name = "Danny Lee", Age = 30 });
+_context.SaveChanges();
+
+// Because DbSet is not IList, index operator will not work
+// var first = _context.Customers[0];
+
+var john = _context.Customers.FirstOrDefault(c => c.Name == "John Doe");
+if (john != null) john.Age = 31;
+
+_context.SaveChanges();
+
 foreach (var customer in customers)
 {
     Console.WriteLine($"Id: {customer.Id} Customer: {customer.Name}, Age: {customer.Age}");
@@ -35,6 +63,7 @@ foreach (var customer in customers)
 class CrmContext : DbContext
 {
     public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -44,7 +73,26 @@ class CrmContext : DbContext
     }
 }
 
-class Customer
+public class Order
+{
+    [Key]
+    public int OrderId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [MinLength(3)]
+    public string Product { get; set; }
+
+    [Required]
+    [Precision(18, 2)]
+    public decimal Price { get; set; }
+
+    [ForeignKey("CustomerId")]
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+}
+
+public class Customer
 {
     public int Id { get; set; }
     public string Name { get; set; }
