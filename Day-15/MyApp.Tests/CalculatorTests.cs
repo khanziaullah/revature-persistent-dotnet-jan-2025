2f1816c619bb1cc23c6f1993de598a1b4b7fd7e7
Ziaullah Khan <zkhan@capacityllc.com>
2026-02-20 11:55:32 +0530

    xunit test case demo Fact, Theory, Inline Data

--- Day-15/MyApp.Tests/CalculatorTests.cs
+++ Day-15/MyApp.Tests/CalculatorTests.cs
@@ -6,8 +6,11 @@ namespace MyApp.Tests;
 
 public class CalculatorTests
 {
-    [Fact]
-    public void Add_TwoNumbers_GivesCorrectResult()
+    [Theory]
+    [InlineData(2, 3, 5)]     // 2 + 3 = 5
+    [InlineData(0, 0, 0)]     // 0 + 0 = 0
+    [InlineData(-1, 1, 0)]    // -1 + 1 = 0
+    public void Add_TwoNumbers_GivesCorrectResult(int x, int y, int expectedResult)
     {
         // Arrange
         var calculator = new Calculator();
@@ -15,12 +18,31 @@ public class CalculatorTests
         // var sut = new Calculator();
 
         // manual calculation
-        var x = 5;
-        var y = 10;
-        var expectedResult = 15;
+        // var x = 5;
+        // var y = 10;
+        // var expectedResult = 15;
 
         // Act
         var actualResult = calculator.Add(x, y);
+
+        // Assert
+        Assert.Equal(expected: expectedResult, actual: actualResult);
+    }
+
+    [Theory]
+    [InlineData(2, 3, -1)]
+    [InlineData(0, 0, 0)]
+    [InlineData(-1, 1, -2)]
+    public void Subtract_TwoNumbers_GiveCorrectResult(int x, int y, int expectedResult)
+    {
+        // Arrange
+        var calculator = new Calculator();
+        // var x = 10;
+        // var y = 5;
+        // var expectedResult = 5;
+
+        // Act
+        var actualResult = calculator.Subtract(x, y);
 
         // Assert
         Assert.Equal(expected: expectedResult, actual: actualResult);
--- Day-15/MyApp/Calculator.cs
+++ Day-15/MyApp/Calculator.cs
@@ -6,4 +6,9 @@ public class Calculator
     {
         return a + b;
     }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
 }
