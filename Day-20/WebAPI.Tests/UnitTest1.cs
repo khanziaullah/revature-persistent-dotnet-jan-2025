2f8aeab30b822dfc635ba10aed53ff991f90875e
Ziaullah Khan <zkhan@capacityllc.com>
2026-03-05 13:14:48 +0530

    Fixed Assert failure for Customer Contrller get method

--- Day-20/WebAPI.Tests/UnitTest1.cs
+++ Day-20/WebAPI.Tests/UnitTest1.cs
@@ -3,20 +3,21 @@ namespace WebAPI.Tests;
 using WebAPI;
 using DataAccessLayer;
 using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
 
 public class UnitTest1
 {
     [Fact]
     //[Theory]
     //[InlineData(1, 2, 3, 4)]
-    public void Customer_CalculateInterest_ReturnsCorrectValue()
+    public async void Customer_CalculateInterest_ReturnsCorrectValue()
     // public void Customer_CalculateInterest_ReturnsCorrectValue(int duration, int principal, int rate, int expectedInterest)
     {
         // Arrange
         // controller, services
         ICustomerService customerService = A.Fake<ICustomerService>();
 
-        A.CallTo(() => customerService.GetAllCustomers()).Returns(new List<Customer>
+        var customers = new List<Customer>
         {
             new Customer
             {
@@ -24,30 +25,31 @@ public class UnitTest1
                 Name = "Sarah Smith",
                 Email = "sarah.smith@example.com"
             }
-        });
+        };
+
+        A.CallTo(() => customerService.GetAllCustomers()).Returns(customers);
 
         var customerController = new CustomerController(customerService);
 
         var expectedResult = new
         {
             StatusCode = 200,
-            Value = new List<Customer> {
-            new Customer
-            {
-                Id = 3,
-                Name = "Sarah Smith",
-                Email = "sarah.smith@example.com"
-            }
-        }
+            Value = customers
         };
 
         // Act
         var actualResult = customerController.Get();
 
+        var okResult = actualResult as OkObjectResult;
+        var actualCustomers = okResult?.Value as List<Customer>;
 
         // Assert
         // Check of Status Code is 200
         // Check of Interest is correct
-        //Assert.Equal(expectedResult.StatusCode, actualResult.StatusCode);
+        Assert.Equal(expectedResult.StatusCode, okResult?.StatusCode);
+
+        // Assert collections are equal
+
+        Assert.Equal(expectedResult.Value, actualCustomers);
     }
 }
