238be627b4768566be3fd8177c904832557c66ff
Ziaullah Khan <zkhan@capacityllc.com>
2026-03-13 14:55:36 +0530

    Observer Pattern

--- Day-25/Docker.sh
+++ Day-25/Docker.sh
@@ -1,2 +1,3 @@
 
 docker run -e ACCEPT_EULA=Y -e MSSQL_SA_PASSWORD="p@ssw0rd" -p 1433:1433 -v crm_sql_volume:/var/opt/mssql --name sqlserver2019 -d mcr.microsoft.com/mssql/server:2019-latest
+docker run -d -p 8500:8500 -p 8600:8600/udp  --name consul  hashicorp/consul agent -dev -client=0.0.0.0
